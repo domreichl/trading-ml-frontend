794c5446e17787a67bb73eba5c5584157b589bef
Dominic Reichl <office@dominicreichl.com>
2023-09-23 15:11:28 +0200

    update trades

--- pages/trades.py
+++ pages/trades.py
@@ -6,17 +6,14 @@ from utils.file_handling import read_csv_file, read_json_file
 add_page_title()
 
 trades = read_csv_file("trades")
-trading_performance = read_json_file("trading_performance")
-model_performance = read_json_file("model_performance")
+tp = read_json_file("trading_performance")
+mp = read_json_file("model_performance")
 
 st.subheader(f"Totals")
 c1, c2, c3 = st.columns(3)
-c1.metric("Volume", "{:.2}€".format(trading_performance["TOTAL_VOLUME"]))
-c2.metric("Volume", trading_performance["TOTAL_VOLUME"])
-#c2.metric("Gross Profit", trading_performance["TOTAL_GROSS_PROFIT"])#"{:.2}€".format(float(trading_performance["TOTAL_GROSS_PROFIT"])))
-c3.metric("Fees", round(trading_performance["TOTAL_FEES"], 2))
-
-st.write(trading_performance["TOTAL_GROSS_PROFIT"])
+c1.metric("Volume", "{:.2}€".format(tp["TOTAL_VOLUME"]))
+c2.metric("Gross Profit", "{:.2}€".format(tp["GROSS_PROFIT"]))
+c3.metric("Fees", "{:.2}€".format(tp["FEES"]))
 
 st.subheader(f"Trades")
 if st.checkbox("Show dataframe"):
@@ -40,20 +37,7 @@ if st.checkbox("Show dataframe"):
 #     "AVG_LOSS": trades_loss["NET_PROFIT"].mean(),
 #     "SQN": df["REWARD"].mean() / df["REWARD"].std() * np.sqrt(len(df)),
 # }
-# print(trading_performance)
-# write_json_results(trading_performance, "trading_performance")
-
-# model_performance = {}
 # for model in df["MODEL"].unique():
-#     df_model = df[df["MODEL"] == model]
-#     buy_price = df_model["BUY_PRICE"].values
-#     sell_price_gt = df_model["SELL_PRICE"].values
-#     sell_price_pr = df_model["PREDICTED_PRICE"].values
-#     win_gt = sell_price_gt > buy_price
-#     win_pr = sell_price_pr > buy_price
-#     precision, recall, f1_score, _ = precision_recall_fscore_support(
-#         win_gt, win_pr, average="binary"
-#     )
 #     model_performance[model] = {
 #         "RETURN_MAE": np.mean(df_model["RETURN_AE"]),
 #         "PRICE_SMAPE": compute_SMAPE(sell_price_pr, sell_price_gt),
