4cbfc35cedfb7e5530e7989b04c1db0c8074da45
Dominic Reichl <office@dominicreichl.com>
2023-09-23 14:46:50 +0200

    update data; continue trades.py

--- TODO.md
+++ TODO.md
@@ -1,7 +1,10 @@
 ## TODO
-1. finish visualization in performance.py and dashboard.py when all models are developed
-2. visualize trading performance in trades.py
-3. complete backtests.py
-4. confusion matrix for sign predictions
-5. add error bars (research how to best compute them with ML models)
-6. weekly update of data for visualizations with auto-commits to prevent the app from going to sleep
+1. visualize trading performance in trades.py
+2. update visualization in performance.py and dashboard.py when all models are developed
+3. finish visualizations:
+    - https://pandas.pydata.org/docs/user_guide/style.html
+    - https://plotly.com/python/plotly-express/
+    - backtests.py
+    - confusion matrix for sign predictions
+    - add error bars (research how to best compute them with ML models)
+4. weekly update of data for visualizations with auto-commits to prevent the app from going to sleep
--- pages/trades.py
+++ pages/trades.py
@@ -8,7 +8,50 @@ add_page_title()
 trading_performance = read_json_file("trading_performance")
 model_performance = read_json_file("model_performance")
 
-a1, a2, a3 = st.columns(3)
-a1.metric("Total Gross Profit", trading_performance["TOTAL_GROSS_PROFIT"])
-a2.metric("Total Net Profit", trading_performance["TOTAL_NET_PROFIT"])
-a3.metric("Total Fees", trading_performance["TOTAL_FEES"])
+st.subheader(f"Totals")
+c1, c2, c3, c4 = st.columns(4)
+c1.metric("Volume", "{:.2}€".format(trading_performance["TOTAL_VOLUME"]))
+c2.metric("Gross Profit", "{:.2}€".format(trading_performance["TOTAL_GROSS_PROFIT"]))
+c3.metric("Net Profit", "{:.2}€".format(trading_performance["TOTAL_NET_PROFIT"]))
+c4.metric("Fees", "{:.2}€".format(trading_performance["TOTAL_FEES"]))
+
+# trading_performance = {
+#     "N_TRADES": len(df),
+#     "N_TRADES_WIN": len(trades_win),
+#     "N_TRADES_LOSS": len(trades_loss),
+#     "WIN_RATE": len(trades_win) / len(df),
+#     "TOTAL_VOLUME": (df["BUY_PRICE"] * df["SHARES"]).sum(),
+#     "TOTAL_GROSS_PROFIT": df["GROSS_PROFIT"].sum(),
+#     "TOTAL_NET_PROFIT": df["NET_PROFIT"].sum(),
+#     "TOTAL_FEES": df["FEES"].sum(),
+#     "AVG_VOLUME": (df["BUY_PRICE"] * df["SHARES"]).mean(),
+#     "AVG_PROFIT": df["NET_PROFIT"].mean(),
+#     "STD_PROFIT": df["NET_PROFIT"].std(),
+#     "MAX_WIN": df["NET_PROFIT"].max(),
+#     "MAX_LOSS": df["NET_PROFIT"].min(),
+#     "AVG_WIN": trades_win["NET_PROFIT"].mean(),
+#     "AVG_LOSS": trades_loss["NET_PROFIT"].mean(),
+#     "SQN": df["REWARD"].mean() / df["REWARD"].std() * np.sqrt(len(df)),
+# }
+# print(trading_performance)
+# write_json_results(trading_performance, "trading_performance")
+
+# model_performance = {}
+# for model in df["MODEL"].unique():
+#     df_model = df[df["MODEL"] == model]
+#     buy_price = df_model["BUY_PRICE"].values
+#     sell_price_gt = df_model["SELL_PRICE"].values
+#     sell_price_pr = df_model["PREDICTED_PRICE"].values
+#     win_gt = sell_price_gt > buy_price
+#     win_pr = sell_price_pr > buy_price
+#     precision, recall, f1_score, _ = precision_recall_fscore_support(
+#         win_gt, win_pr, average="binary"
+#     )
+#     model_performance[model] = {
+#         "RETURN_MAE": np.mean(df_model["RETURN_AE"]),
+#         "PRICE_SMAPE": compute_SMAPE(sell_price_pr, sell_price_gt),
+#         "ACCURACY": accuracy_score(win_gt, win_pr),
+#         "PRECISION": precision,
+#         "RECALL": recall,
+#         "F1": f1_score,
+#     }
