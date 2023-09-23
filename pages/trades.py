18679e2f6860958cada9f19e4db6909631fc89c3
Dominic Reichl <office@dominicreichl.com>
2023-09-23 15:05:41 +0200

    update trades

--- pages/trades.py
+++ pages/trades.py
@@ -12,7 +12,7 @@ model_performance = read_json_file("model_performance")
 st.subheader(f"Totals")
 c1, c2, c3 = st.columns(3)
 c1.metric("Volume", trading_performance["TOTAL_VOLUME"])
-c2.metric("Gross Profit", "{:.2}€".format(trading_performance["TOTAL_GROSS_PROFIT"]))
+c2.metric("Gross Profit", "{:.2}€".format(float(trading_performance["TOTAL_GROSS_PROFIT"])))
 c3.metric("Fees", round(trading_performance["TOTAL_FEES"], 2))
 
 st.subheader(f"Trades")
