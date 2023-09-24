6f94611a58ba243a261797a5c8b61bff063a89b7
Dominic Reichl <office@dominicreichl.com>
2023-09-24 13:07:32 +0200

    update model comparison

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import streamlit as st
 from st_pages import add_page_title
 
@@ -9,33 +10,36 @@ st.sidebar.markdown(
     "Warning: The measures you see here are of dummy models used solely for showcasing purposes."
 )
 
-metrics_available = ["all", "SMAPE", "SMAPE_last", "MAE", "MASE"]
+st.subheader("Test Performance")
+metrics_available = ["all", "SMAPE", "MAE", "MASE"]
 metrics_captions = [
     "show all metrics",
     "Symmetric Mean Absolute Percentage Error",
-    "SMAPE for the final prediction",
+    # "SMAPE for the final prediction",
     "Mean Absolute Error",
     "Mean Absolute Scaled Error",
 ]
 metric_selected = st.radio(
-    "Select performance metric",
+    "Select metric",
     options=metrics_available,
-    captions=metrics_captions,
+    # captions=metrics_captions,
+    horizontal=True,
 )
 
-performance = read_csv_file("performance_regression")
-performance = performance[
-    performance["Target"].isin(["Price", "Log Return"])
-    & performance["Metric"].isin(metrics_available)
+rp = read_csv_file("performance_regression")
+rp = rp[
+    rp["Target"].isin(["Price", "Log Return"]) & rp["Metric"].isin(metrics_available)
 ]
 
 if metric_selected == "all":
-    st.dataframe(performance)
+    st.dataframe(rp)
 else:
-    st.dataframe(performance[performance["Metric"] == metric_selected])
+    st.dataframe(rp[rp["Metric"] == metric_selected])
 
-
+st.subheader("Trading Performance")
 mp = read_json_file("model_performance")
+model_selected = st.selectbox("Select model", list(mp.keys()))
+st.dataframe(pd.DataFrame(mp[model_selected]))
 # TODO: model-specific trading performance
 # for model in df["MODEL"].unique():
 #     model_performance[model] = {
